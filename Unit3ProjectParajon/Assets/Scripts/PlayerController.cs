a4f311e108f17388b1ad7defb215f4552da12fd9
IceBear2491 <salvadorparajon5@gmail.com>
2026-03-26 15:26:26 -0700

    Did like the whole game except particles and sounds
    2

--- Unit3ProjectParajon/Assets/Scripts/PlayerController.cs
+++ Unit3ProjectParajon/Assets/Scripts/PlayerController.cs
@@ -2,29 +2,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public bool gameOver = false;
+
     private Rigidbody playerRb;
     public float jumpForce = 10;
     public float gravityModifier;
-    public bool isOnGround = true; 
+    public bool isOnGround = true;
+    private Animator playerAnim;
+
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         playerRb = GetComponent<Rigidbody>();
+        playerAnim = GetComponent<Animator>();
         Physics.gravity *= gravityModifier;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetKeyDown(KeyCode.Space) && isOnGround)
+        if (Input.GetKeyDown(KeyCode.Space) && isOnGround && !gameOver)
         {
             playerRb.AddForce(Vector3.up * 10, ForceMode.Impulse);
             isOnGround = false;
+            playerAnim.SetTrigger("Jump_trig");
         }
     }
     private void OnCollisionEnter(Collision collision)
     {
-        isOnGround = true;
+        if (collision.gameObject.CompareTag("Ground"))
+        {
+            isOnGround = true;
+        }
+        else if (collision.gameObject.CompareTag("Obstacle"))
+        {
+            gameOver = true;
+            Debug.Log("Game Over!");
+            playerAnim.SetBool("Death_b", true);
+            playerAnim.SetInteger("DeathType_int", 1);
+        }
+
     }
 }
